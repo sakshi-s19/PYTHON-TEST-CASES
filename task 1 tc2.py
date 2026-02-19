190e7526c4a9ad13aae6878d1eba0bf13d1b3b84
sakshi <kppmpramod321@gmail.com>
2026-02-19 20:39:30 +0530

    TASK 1 TC-2 ADDED

---
+++ task 1 tc2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Test Case 2
+print("Test Case 2")
+
+# Create two 1D arrays
+array1 = np.array([2, 4, 6, 8])
+array2 = np.array([1, 3, 5, 7])
+
+# Element-wise multiplication
+result = array1 * array2
+
+# Display results
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("Element-wise Multiplication Result:", result)
